8fb9f43a3bbcfc46ee7e319992f64aef59b08041
timjar3 <84810351+timjar3@users.noreply.github.com>
2022-04-01 17:41:45 +0100

    Update ConditionalExpressions.tf

--- ConditionalExpressions/ConditionalExpressions.tf
+++ ConditionalExpressions/ConditionalExpressions.tf
@@ -7,15 +7,19 @@ provider "aws" {
 
 variable "istest"{}
 
+# if QAenv == true, then creat this resource       QA environment or Dev environment
+
 # ec2-instance for test
 resource "aws_instance" "test" {
   ami = "ami-0800fc0fa715fdcfe"
   instance_type = "t2.micro"
   count=var.istest == true ? 1 : 0
-  # if the value of variable istest is true, than the count will be set to 1
+  # if the value of variable istest is true, than the count will be set to 1 (here the count 1 can be modified)
   # and one insatce of this resource will be created.
   }
+
 # ec2-instance for prod
+# if QAenv == false, then creat this resource        QA environment or Dev environment
 resource "aws_instance" "prod" {
     ami = "ami-0800fc0fa715fdcfe"
     instance_type = "t2.large"
